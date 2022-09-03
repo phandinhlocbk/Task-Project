2e39bef1eb6c1a23d97f91345fc57748fdc8b4df
Loc Phan <locphan@Locs-MacBook-Air.local>
2022-09-03 15:07:21 +0900

    user profile page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 class AdminController extends Controller
@@ -20,4 +21,37 @@ class AdminController extends Controller
 
         return redirect('/login');
     }//end method
+
+    public function Profile() {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view', compact('adminData'));
+    }//end method
+
+    public function EditProfile() {
+        $id = Auth::user()->id;
+        $editData = User::find($id);
+        return view('admin.admin_profile_edit', compact('editData'));
+    }//end method
+
+    public function StoreProfile(Request $request) {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+       
+
+        if ($request->file('profile_image')) {
+           $file = $request->file('profile_image');
+
+           $filename = date('YmdHi').$file->getClientOriginalName();
+           $file->move(public_path('upload/admin_images'),$filename);
+           $data['profile_image'] = $filename;
+        }
+        $data->save();
+
+        return redirect()->route('admin.profile');
+    }
+
+
 }
